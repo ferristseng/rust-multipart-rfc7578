c6d1491491f0d97478a3c5f1a6d49dc79bba213e
Ferris Tseng <ferristseng@fastmail.fm>
2017-11-24 13:09:05 -0500

    add license, and dependencies

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+// Copyright 2017 rust-hyper-multipart-rfc7578 Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+//
+
+extern crate bytes;
+extern crate hyper;
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
