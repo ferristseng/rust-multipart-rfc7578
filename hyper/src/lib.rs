d90bd02d0b0379090d1d97f04cf983c379e9f060
Ferris Tseng <ferristseng@fastmail.fm>
2019-02-08 18:12:53 -0500

    documentation; moving conflicting example name

--- hyper/src/lib.rs
+++ hyper/src/lib.rs
@@ -6,6 +6,91 @@
 // copied, modified, or distributed except according to those terms.
 //
 
+//! This crate contains an implementation of the multipart/form-data media
+//! type described in [RFC 7578](https://tools.ietf.org/html/rfc7578) for
+//! [hyper](https://hyper.rs/).
+//!
+//! ## Usage
+//!
+//! Declare the dependency:
+//!
+//! ```toml
+//! [dependencies]
+//! hyper-multipart-rfc7578 = "0.1.0"
+//! ```
+//!
+//! Import the crate:
+//!
+//! ```rust
+//! extern crate hyper_multipart_rfc7578 as multipart;
+//! ```
+//!
+//! ## Example:
+//!
+//! With a custom client:
+//!
+//! ```rust
+//! # extern crate hyper;
+//! # extern crate hyper_multipart_rfc7578 as hyper_multipart;
+//!
+//! use hyper::{
+//!     rt::{self, Future},
+//!     Client, Request,
+//! };
+//! use hyper_multipart::client::{self, multipart};
+//!
+//! # fn main() {
+//! let client = Client::builder().build_http();
+//! let mut form = multipart::Form::default();
+//!
+//! form.add_text("test", "Hello World");
+//!
+//! let mut req_builder = Request::get("http://localhost/upload");
+//!
+//! let req = form.set_body::<multipart::Body>(&mut req_builder).unwrap();
+//!
+//! rt::run(
+//!     client
+//!         .request(req)
+//!         .map(|_| println!("done..."))
+//!         .map_err(|_| println!("an error occurred")),
+//! );
+//! # }
+//! ```
+//!
+//! With a default client:
+//!
+//! ```rust
+//! # extern crate hyper;
+//! # extern crate hyper_multipart_rfc7578 as hyper_multipart;
+//!
+//! use hyper::{
+//!     rt::{self, Future},
+//!     Client, Request,
+//! };
+//! use hyper_multipart::client::{self, multipart};
+//!
+//! # fn main() {
+//! let client = Client::new();
+//! let mut form = multipart::Form::default();
+//!
+//! form.add_text("test", "Hello World");
+//!
+//! let mut req_builder = Request::get("http://localhost/upload");
+//!
+//! let req = form.set_body_convert::<hyper::Body, multipart::Body>(&mut req_builder)
+//!     .unwrap();
+//!
+//! rt::run(
+//!     client
+//!         .request(req)
+//!         .map(|_| println!("done..."))
+//!         .map_err(|_| println!("an error occurred")),
+//! );
+//! # }
+//! ```
+//!
+
 extern crate bytes;
 extern crate common_multipart_rfc7578 as common_multipart;
 extern crate futures;
